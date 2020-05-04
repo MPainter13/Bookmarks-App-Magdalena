986137bdf881a8e77129d5af6f1f6a08c0de4b68
Magdalena Painter <paintermagdalena@gmail.com>
2020-05-03 21:10:27 -0400

    in progress

--- api.js
+++ api.js
@@ -1,7 +1,2 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/Magdalena';
+// const BASE_URL = 'https://thinkful-list-api.herokuapp.com/Magdalena';
 
-const getItems
-
-const createItem
-
-const deleteItem
--- index.js
+++ index.js
@@ -26,30 +26,47 @@ const store = {
 
 const main = function() {
  addBookmark();
+ handleNewItemSubmit();
  filterBookmark();
+ handleCancelButton();
+ expendItem();
  render();
 }
  
 const render = function() {
   const items = store.bookmarks.map(bookmark =>{
-    return `
-    <li class="title">
+    if (bookmark.expanded) {
+      return `
+      <li class="bookmark-item" data-id="${bookmark.id}">
+      <h2>${bookmark.title}</h2> <div class="rating">${bookmark.rating}</div>
+      <p> ${bookmark.description}</p>
+    </li>
+      `; 
+    }
+    else {
+      return `
+    <li class="bookmark-item" data-id="${bookmark.id}">
     <h2>${bookmark.title}</h2> <div class="rating">${bookmark.rating}</div>
   </li>
     `;
+    }
+    
   });
 
   let adding = '';
-  if (store.adding == true) {
+  if (store.adding === true) {
     adding = `
-    <form class="addingItem>
+    <form class="addingItem js-addingItem">
     <label for="addNewBookmark">Add new Bookmark</label>
-    <input type="text" id="addNewBookmark" name="title">
+    <input type="text" id="addNewBookmark" name="title" required>
     <label for="addDescription">Description</label>
     <input type="text" id="description" name="description">
     <label for="link">Link:</label>
-    <input type="url" id="link" name="link">
-    <button>Submit</button>
+    <input type="url" id="link" name="url" required>
+    <label for="rating">Rating:</label>
+    <input type="text" id="rating" name="rating">
+    <button type="submit" class="submit">Submit</button>
+    <button type="button" id="js-cancel">Cancel</button>
     </form>
     
     `;
@@ -71,11 +88,48 @@ const render = function() {
   $('main').html(html);
 }
 
+
+
+const expendItem = function() {
+ $('main').on('click', '.bookmark-item', event => {
+  const id = $(event.target).data('id')
+  const item = store.bookmarks.find(bookmark => bookmark.id === id);
+ if (!item.expanded) {
+   item.expanded = true;
+ }
+ else {
+   item.expanded = false;
+ }
+  render();
+ })
+};
+
 const addBookmark = function() {
  $('main').on('click', '#addBookmark', e => {
    store.adding = true;
   render();
  });
+};
+
+const handleNewItemSubmit = function () {
+  $('main').on('submit', '.js-addingItem', event => {
+    event.preventDefault();
+    const item = {
+      title: event.target.title.value,
+      rating: event.target.rating.value,
+      url: event.target.url.value,
+      description: event.target.description.value
+    };
+    store.bookmarks.push(item);
+    render();
+  });
+};
+
+const handleCancelButton = function () {
+  $('main').on('click', '#js-cancel', event => {
+    store.adding = false;
+    render();
+  })
 }
 
 const filterBookmark = function() {
@@ -84,9 +138,5 @@ const filterBookmark = function() {
  });
 }
 
-const expendItem = function() {
-
-};
 
 $(main);
-
